65bbd308e45e52fde3ea739c8dd7aea809aba3ce
ryo-ma <saka_ro@yahoo.co.jp>
2021-11-29 01:51:48 +0900

    Add chakra-ui

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Dashboard from "../components/Dashboard"
+import NavBar from '../components/NavBar'
 
 const Home: NextPage = () => {
   return (
-    <Dashboard/>
+    <NavBar/>
   )
 }
 
