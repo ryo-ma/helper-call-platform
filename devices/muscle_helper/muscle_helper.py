3afa2d270f332f8f42192f000896980b61004e64
ryo-ma <saka_ro@yahoo.co.jp>
2021-12-02 20:57:08 +0900

    Update muscle_helper

--- devices/muscle_helper/muscle_helper.py
+++ devices/muscle_helper/muscle_helper.py
@@ -1,12 +1,14 @@
 import sys
 import time
 import RPi.GPIO as GPIO
+from call_client import CallClient
 
 SPI_CLK = 11
 SPI_MOSI = 10
 SPI_MISO = 9
 SPI_SS = 8
 CHANNEL = 0
+RESULT_LIST_SIZE = 25
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -18,6 +20,9 @@ GPIO.setup(SPI_SS,   GPIO.OUT)
 
 
 class MuscleHelper:
+    def __init__(self):
+        self.reset_result_list()
+
     # Send the channel to AD converter.
     def send_channel(self, channel):
         GPIO.output(SPI_SS,   False)
@@ -39,6 +44,9 @@ class MuscleHelper:
         GPIO.output(SPI_CLK, True)
         GPIO.output(SPI_CLK, False)
 
+    def reset_result_list(self):
+        self.result_list = [0] * RESULT_LIST_SIZE
+
     def receive_result(self):
         value = 0
         # Receive a result of 12bit from AD converter.
@@ -55,11 +63,19 @@ class MuscleHelper:
     def run(self):
         self.send_channel(CHANNEL)
         result = self.receive_result()
-        sys.stdout.write(str(result))
-        sys.stdout.write("\n")
+        self.result_list.append(result)
+        self.result_list.pop(0)
+        print(self.result_list)
+        return all([x > 3500 for x in self.result_list])
 
 
 if __name__ == '__main__':
+    muscle_helper = MuscleHelper()
+    call_client = CallClient()
+
     while(True):
         time.sleep(0.1)
-        MuscleHelper().run()
+        if muscle_helper.run():
+            call_client.call(call_client.get_token())
+            muscle_helper.reset_result_list()
+            time.sleep(5)
